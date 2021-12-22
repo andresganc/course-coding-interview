1716b97d157fbb71888dd30811f0ace860106e1d
Andres Giraldo <andresganc@gmail.com>
2021-12-21 19:12:16 -0500

    New: Negative Infinity

---
+++ exercises-typescript/1-basic/negative-infinity.js
@@ -0,0 +1,12 @@
+
+function find_max(nums) {
+    let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
+    for (let num of nums) {
+        if (num > max_num) {
+            // (Fill in the missing line here)
+        }
+    }
+    return console.log(max_num);
+}
+
+find_max([5, -3, 9, 4])
