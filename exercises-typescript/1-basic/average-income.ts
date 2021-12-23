86fb1039604e9ccf954f8f6f94d2d1011b41fcc4
Andres Giraldo <andresganc@gmail.com>
2021-12-23 12:46:08 -0500

    New: Average Income

---
+++ exercises-typescript/1-basic/average-income.ts
@@ -0,0 +1,16 @@
+
+/*
+What is the average income of all the people in the array?
+*/
+
+// Array 1
+let array1: unknown = [
+    { name: 'Andres', lastname: 'Giraldo', salary: 25000000 },
+    { name: 'Daniela', lastname: 'Giraldo', salary: 15000000 },
+    { name: 'Marco', lastname: 'Giraldo', salary: 10000000 },
+]
+
+const averageIncome = ( array1: unknown[] ) => {
+    return 
+}
+
